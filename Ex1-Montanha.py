28c8543c45ab13c94cbdfd3a6f80d1b59b8f451a
Arthur Lima <arthur-esc@hotmail.com>
2018-03-26 12:36:06 -0300

    Exercício 1

--- Ex1-Montanha.py
+++ Ex1-Montanha.py
@@ -6,6 +6,8 @@ countM = 0
 countF = 0
 somaalturaM = 0
 somaalturaF = 0
+mediaF = 0
+mediaM = 0
 maior = 0
 
 for i in range(qtd):
@@ -14,24 +16,26 @@ for i in range(qtd):
     
   alturaTotal = alturaTotal + altura[i]
   if i == 0:
-    maior = altura
+    maior = altura[i]
   
-  if sexo[i] == 'M':
+  if sexo[i].lower() == 'm':
     countM = countM + 1
     somaalturaM = somaalturaM + altura[i]
   
-  if sexo[i] == 'F':
+  if sexo[i].lower() == 'f':
     countF = countF + 1
     somaalturaF = somaalturaF + altura[i]
 
-  if altura > maior:
-    maior = altura
+  if altura[i] > maior:
+    maior = altura[i]
 
 mediaPessoas = alturaTotal/qtd
-mediaF = somaalturaF/countF
-mediaM = somaalturaM/countM
+if countF > 0:
+  mediaF = somaalturaF/countF
+if countM > 0:
+  mediaM = somaalturaM/countM
 
-
+print('\n\n')
 print('Altura média das pessoas: ',mediaPessoas)
 print('A maior altura é: ', maior)
 print('Altura media das Mulheres: ',mediaF)
